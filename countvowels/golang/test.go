df91ddb482f84fb5a0e6d4501621144be0672242
Dean <agandfr@gmail.com>
2015-12-10 22:06:14 +1100

    Updated go lang version

--- countvowels/golang/test.go
+++ countvowels/golang/test.go
@@ -5,13 +5,13 @@ import "fmt"
 import "strings"
 
 func Filter(vs []string, f func(string) bool) []string {
-    vsf := make([]string, 0)
-    for _, v := range vs {
-        if f(v) {
-            vsf = append(vsf, v)
+    splittedstrings := make([]string, 0)
+    for _, ch := range vs {
+        if f(ch) {
+            splittedstrings = append(splittedstrings, ch)
         }
     }
-    return vsf
+    return splittedstrings
 }
 
 func countvowels(inputStr string) int {
