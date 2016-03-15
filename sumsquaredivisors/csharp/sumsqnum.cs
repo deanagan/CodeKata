4d8015574da795466d2bb137af66adbb2963df14
Dean <agandfr@gmail.com>
2016-03-15 21:00:30 +1100

    Optimized solution to sumsqnum.cs

--- sumsquaredivisors/csharp/sumsqnum.cs
+++ sumsquaredivisors/csharp/sumsqnum.cs
@@ -1,23 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public class SumSquaredDivisors 
 {
+    public static void Main(string[] args)
+    {
+    }
 		
-	public static string listSquared(long m, long n)
+    public static IEnumerable<long> GetRange(long g, long h)
+    {
+        long sqrt = (long)Math.Sqrt(h) + 1;
+        HashSet<long> yielded = new HashSet<long>();
+        for (long e = g; e <= sqrt; ++e)
+        {
+            if (h % e == 0)
+            {
+                if (!yielded.Contains(e))
+                {
+                    yield return e*e;
+                    yielded.Add(e);
+                }
+                long md = h/e;
+                if (!yielded.Contains(md))
+                {
+                    yield return md*md;
+                    yielded.Add(md);
+                }
+            }
+        }
+    }    
+    public static string listSquared(long m, long n)
 	{
     string result = "[{0}]";
     List<string> items = new List<string>();
-		for (long numbers = m; numbers <= n; ++numbers)
+    for (long numbers = m; numbers <= n; ++numbers)
     {
-      long sumSquared = Enumerable.Range(1, numbers)
-                                  .Where(divisor=> numbers%divisor == 0)
-                                  .Select(sqnum=>sqnum*sqnum).Sum(ss=>ss);
+      long sumSquared = GetRange(1, numbers).Sum(ss=>ss);
       
       if (Math.Sqrt(sumSquared) % 1 == 0)
       {
-        items.Add(String.Format("[{0}, {1}]", numbers, sumSquared);  
+        items.Add(String.Format("[{0}, {1}]", numbers, sumSquared));  
       }
     }
     
-    return String.Join(", ", items);
+    return String.Format(result, String.Join(", ", items));
 	}
+
+
 }
+
+
 
