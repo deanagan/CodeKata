78bc5683902563c26e55a96c2034c448204ca06e
Dean Agan <agandfr@gmail.com>
2019-10-28 10:44:43 +1100

    Add csharp solution for remove_string_spaces

---
+++ remove_string_spaces/csharp/solution/solution.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Solution
+{
+  public static class SpacesRemover
+  {
+    public static string NoSpace(string input)
+    {
+        return string.Concat(from ch in input
+                where ch != ' '
+                select ch);
+    }
+  }
+}
