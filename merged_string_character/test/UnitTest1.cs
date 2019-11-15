cc7bfe28ae3e832604b79a59f821662cdef1b181
Dean Agan <agandfr@gmail.com>
2019-11-15 12:29:53 +1100

    Add string merger solution. Still failing

--- merged_string_character/test/UnitTest1.cs
+++ merged_string_character/test/UnitTest1.cs
@@ -16,22 +16,40 @@ namespace test
             Assert.IsTrue(StringMerger.isMerge("codewars", "code", "wars"), "codewars can be created from code and wars");
         }
 
-        //[Test]
-        // public void HappyPath2()
-        // {
-        //     Assert.IsTrue(StringMerger.isMerge("codewars", "cdwr", "oeas"), "codewars can be created from cdwr and oeas");
-        // }
+        [Test]
+        public void HappyPath2()
+        {
+            Assert.IsTrue(StringMerger.isMerge("codewars", "cdwr", "oeas"), "codewars can be created from cdwr and oeas");
+        }
 
-        // [Test]
-        // public void SadPath1()
-        // {
-        //     Assert.IsFalse(StringMerger.isMerge("codewars", "cod", "wars"), "Codewars are not codwars");
-        // }
+        [Test]
+        public void SadPath1()
+        {
+            Assert.IsFalse(StringMerger.isMerge("codewars", "cod", "wars"), "Codewars are not codwars");
+        }
 
-        // [Test]
-        // public void Bananas()
-        // {
-        //     Assert.IsTrue(StringMerger.isMerge("Bananas from Bahamas", "Bahas", "Bananas from am"), "Going Bananas!");
-        // }
+        [Test]
+        public void Bananas()
+        {
+            Assert.IsTrue(StringMerger.isMerge("Bananas from Bahamas", "Bahas", "Bananas from am"), "Going Bananas!");
+        }
+
+        [Test]
+        public void BananasR1()
+        {
+            Assert.IsTrue(StringMerger.isMerge("Bananas from Bahamas", "annasomBhma", "Ba fr aas"), "Going Bananas!");
+        }
+
+        [Test]
+        public void BananasR2()
+        {
+            Assert.IsTrue(StringMerger.isMerge("Bananas from Bahamas", "Ba fr aas", "annasomBhma"), "Going Bananas!");
+        }
+
+        [Test]
+        public void BananasR3()
+        {
+            Assert.IsTrue(StringMerger.isMerge("Bananas from Bahamas", "an m Baha", "Banasfromas"), "Going Bananas!");
+        }
     }
 }
